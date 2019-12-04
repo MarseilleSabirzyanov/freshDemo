b1cb9398ab34858e3df158ebca8f782ac6f0890b
Сабирзянов Марсель Фиргатович <MSabirzjanov@kmpo.ru>
2019-12-04 11:21:48 +0300

    Initial commit

---
+++ src/main/java/com/example/freshman/GreetingController.java
@@ -0,0 +1,63 @@
+package com.example.freshman;
+
+import com.example.freshman.domain.Message;
+import com.example.freshman.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+        return "greeting";
+    }
+
+    @GetMapping("/main")
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/main")
+    public String add(
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag);
+
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter.isBlank() || filter.isEmpty())
+            messages = messageRepo.findAll();
+        else
+            messages = messageRepo.findByTag(filter);
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+}
---
+++ src/main/java/com/example/freshman/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.freshman.domain;
+
+public enum Role {
+    USER
+}
